29b207b789e62c6232af71131512b73c68d52a6e
Patrick Dolan <dolanp1992@gmail.com>
2022-03-13 15:04:06 -0700

    add course class and properties
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

---
+++ UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Models
+{
+  public class Course
+  {
+    public Course()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+
+    public int CourseId { get; set; }
+    public string Name { get; set; }
+    public string Number { get; set; }
+
+    public virtual ICollection<CourseStudent> JoinEntities { get; }
+  }
+}
