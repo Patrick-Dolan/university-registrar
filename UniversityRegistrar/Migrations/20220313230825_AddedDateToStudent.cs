76a8f225a7bec3b79a243e42afd751bf63f532ed
Patrick Dolan <dolanp1992@gmail.com>
2022-03-13 16:11:44 -0700

    update database migration to have student date property
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

---
+++ UniversityRegistrar/Migrations/20220313230825_AddedDateToStudent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class AddedDateToStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Students",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Students");
+        }
+    }
+}
