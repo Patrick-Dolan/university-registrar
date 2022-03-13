f1e2f240fcdb79ee3176ef815584e9faea13a70a
Patrick Dolan <dolanp1992@gmail.com>
2022-03-13 15:13:11 -0700

    configure relationship of tables and add lazy loading
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

--- UniversityRegistrar/Models/UniversityRegistrarContext.cs
+++ UniversityRegistrar/Models/UniversityRegistrarContext.cs
@@ -4,7 +4,14 @@ namespace UniversityRegistrar.Models
 {
   public class UniversityRegistrarContext : DbContext
   {
-    public DbSet<DatabaseTablePlaceholder> DatabaseTablePlaceholder { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<CourseStudent> CourseStudents { get; set; }
     public UniversityRegistrarContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
