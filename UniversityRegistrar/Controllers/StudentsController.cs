09908589191fbc7791e41201a8580a58d4723a2d
Patrick Dolan <dolanp1992@gmail.com>
2022-03-14 18:49:17 -0700

    add index view and actionresult for students
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using UniversityRegistrar.Models;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Students.ToList());
+    }
+  }
+}
