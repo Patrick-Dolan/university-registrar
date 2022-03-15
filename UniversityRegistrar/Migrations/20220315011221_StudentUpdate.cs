f1daeb42e88680f3be9daa8e7438982d20c79bd2
Patrick Dolan <dolanp1992@gmail.com>
2022-03-14 18:14:02 -0700

    add new migrations for dateofenrollment property change

---
+++ UniversityRegistrar/Migrations/20220315011221_StudentUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class StudentUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Students",
+                newName: "DateOfEnrollment");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfEnrollment",
+                table: "Students",
+                newName: "Date");
+        }
+    }
+}
